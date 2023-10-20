ba0be3d8d943f29fa56e8fe4e3ca8328c88f5f0f
geek022 <aboutou.ouattara@etudiant.univ-rennes1.fr>
2023-10-20 16:04:34 +0200

    Premier formulaire et traitement

--- premier.php
+++ premier.php
@@ -2,6 +2,10 @@
 <body>
 <?php
 echo "Mon premier script PHP!";
+echo "<br>";
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
 ?>
 </body>
 </html>
---
+++ welcome.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Welcome <?php echo $_GET["txtName"]; ?><br>
+Your email address is: <?php echo $_GET["txtEmail"]; ?>
+
+</body>
+</html>
